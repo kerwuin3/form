827f670932017d0a3b20a0e4f0faf140fab18d45
kerwuin3 <kerwuin1992@outlook.com>
2026-05-08 19:38:32 -0400

    Primer commit

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        
+        <h1>Hola Mundo</h1>
       </main>
     </div>
   );
